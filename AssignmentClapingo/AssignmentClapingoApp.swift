f60382b1c37cf69c0bcf32f8c2becaa74aee95b0
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-08 12:41:57 +0530

    Initial Commit

---
+++ AssignmentClapingo/AssignmentClapingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentClapingoApp.swift
+//  AssignmentClapingo
+//
+//  Created by Manvendu Pathak on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentClapingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
